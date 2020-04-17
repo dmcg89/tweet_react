6a1407a9a9e7108ec000c5a2e99b2e0f8642080c
Michael McGowan <michael.mcgowan@students.makeschool.com>
2020-04-17 13:17:09 -0700

    Updates

---
+++ precache-manifest.b4a41afd2cccda13fafa6cb0e04b5987.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "073151e5dd3b9d1470c51d4782fbd01d",
+    "url": "/tweet_react/index.html"
+  },
+  {
+    "revision": "ba91acb67047a506761a",
+    "url": "/tweet_react/static/css/main.3824d4d5.chunk.css"
+  },
+  {
+    "revision": "bd4da6ab1d9e405c24fb",
+    "url": "/tweet_react/static/js/2.e552c0ac.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tweet_react/static/js/2.e552c0ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba91acb67047a506761a",
+    "url": "/tweet_react/static/js/main.948f8d9c.chunk.js"
+  },
+  {
+    "revision": "f37e4ab2676964c4ed9a",
+    "url": "/tweet_react/static/js/runtime-main.33f2bea1.js"
+  }
+]);
